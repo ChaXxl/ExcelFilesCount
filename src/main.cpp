4703018198ad3f2c391575a08ec86b74f05175e2
ChaXxl <x734461204@gmail.com>
2024-12-14 10:41:36 +0800

    style: 调整代码缩进格式

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+            &engine,
+            &QQmlApplicationEngine::objectCreationFailed,
+            &app,
+            []() { QCoreApplication::exit(-1); },
+            Qt::QueuedConnection);
     engine.loadFromModule("ExcelFilesCount", "Main");
 
     return app.exec();
